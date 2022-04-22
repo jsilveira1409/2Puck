01b033ceeeb19f161cb500ffc85527995c0b451a
jsilveira <joaquim.silveira@epfl.ch>
2022-04-22 11:27:32 +0200

    working

--- 2Puck/pathing.c
+++ 2Puck/pathing.c
@@ -9,7 +9,14 @@
 #include <motors.h>
 #include <arm_math.h>
 
+/*
+ * For the position and target vectors
+ */
+enum {X_AXIS, Y_AXIS};
 
+/*
+ * For the IR obstacle position relative to the puck
+ */
 enum { 	hard_left = 0, 	soft_left, straight_left,
 		straight_right, soft_right, hard_right, none};
 
@@ -21,22 +28,36 @@ enum { 	hard_left = 0, 	soft_left, straight_left,
 #define WHEEL_PERIMETER	130		//in mm
 #define NB_OF_PHASES	4
 #define RAD2DEG			(360/3.14)
-
+#define ANGLE_EPSILON	0.05
+#define MIN_DISTANCE	5
 /*
  * Position values are in mm
  */
 
-#define TARGET_X = 200
-#define TARGET_Y = 200
+#define TARGET_X	(100)		//ptn de parenthese merci pour la nuit blanche
+#define TARGET_Y	300
+
+
 
 /*
  * Current position of the puck, center of axis of the wheels
  */
-volatile  static float pos_x = 0;
-volatile static float pos_y = 0;
-volatile static float angle = 0;
+volatile static float position[2] = {0,0};
 
-static uint8_t arrived = 0;
+/*
+ * LA COORDONNE X SEMBLE INVERSé, PAS COMPRIS POURQUOI ENCORE
+ */
+volatile static float target[2] = {TARGET_X,TARGET_Y};
+volatile static float orientation[2] = {0,0};
+
+/*
+ * Angle between puck's orientation and vector between itself
+ * and the target point
+ */
+volatile static float alpha = 0;
+
+
+
 /*
  * PID instance for the PID cmsis
  */
@@ -51,39 +72,45 @@ static arm_pid_instance_f32 pid;
 static THD_WORKING_AREA(pathingWorkingArea, 256);
 
 static THD_FUNCTION(pathing, arg) {
-
-
+	uint8_t arrived = 0;
+	move(5,-5);
+	move(-10,10);
 	while (true) {
-		while(pos_x <= 100){
 
+		while(arrived == 0){
 			int ir_max = 0;
 			uint8_t ir_index = 0;
 			ir_index = radar(&ir_max);
 
 			switch(ir_index){
 				case none:
-					move(10,10);
+					/*
+					 * No obstacles in front, so move_to_target should
+					 * be called here, move is called inside move_to_target
+					 * in this case
+					 */
+					move_to_target(&arrived);
 					break;
 				case hard_left:
-					move(10,0);
+					move(3, 1);
 					break;
 				case soft_left:
-					move(5,0);
+					move(2, 0);
 					break;
 				case straight_left:
-					move(10,-10);
+					move(2,-3);
 					break;
 				case straight_right:
-					move(-10,10);
+					move(-3,2);
 					break;
 				case soft_right:
-					move(0,5);
+					move(0,2);
 					break;
 				case hard_right:
-					move(0,10);
+					move(1,3);
 					break;
 				default:
-					move(0,0);
+					move(5,5);
 					break;
 			}
 		}
@@ -112,7 +139,7 @@ uint8_t radar(int* ir_max){
 	}
 	*ir_max = max;
 
-	if(max < 250){
+	if(max < 200){
 		return none;
 	}else{
 		return index_max;
@@ -123,7 +150,7 @@ uint8_t radar(int* ir_max){
 /*
  * Implements the PID for the motors, parameters in mm
  */
-void move( float left_pos, float right_pos){
+void move (float left_pos, float right_pos){
 	uint8_t state = 0;
 	right_motor_set_pos(0);
 	left_motor_set_pos(0);
@@ -152,27 +179,27 @@ void move( float left_pos, float right_pos){
 		arm_abs_f32(&error_right,&error_right,1);
 		arm_abs_f32(&error_left,&error_left,1);
 
-		if( error_right >= 2){
-			if(output_right < 250 && output_right > 0){
-				right_motor_set_speed(250);
-			}else if (output_right > -250 && output_right < 0){
-				right_motor_set_speed(-250);
+		if( error_right >= 5){
+			if(output_right < 300 && output_right >= 0){
+				right_motor_set_speed(300);
+			}else if (output_right > -300 && output_right <= 0){
+				right_motor_set_speed(-300);
 			}else{
 				right_motor_set_speed((int)output_right);
 			}
 		}
 
-		if(error_left >= 2){
-			if(output_left < 250 && output_left > 0){
-				left_motor_set_speed(250);
-			}else if(output_left < -250 && output_left < 0){
-				left_motor_set_speed(-250);
+		if(error_left >= 5){
+			if(output_left < 300 && output_left >= 0){
+				left_motor_set_speed(300);
+			}else if(output_left > -300 && output_left <= 0){
+				left_motor_set_speed(-300);
 			}else{
 				left_motor_set_speed((int)output_left);
 			}
 		}
 
-		if(error_left < 2 && error_right < 2){
+		if(error_left < 5 && error_right < 5){
 			state = 1;
 			right_motor_set_speed(0);
 			left_motor_set_speed(0);
@@ -188,27 +215,97 @@ void move( float left_pos, float right_pos){
  */
 
 void register_path( float left_pos,  float right_pos){
-	volatile float displacement = (float)(left_pos + right_pos)/2;						//Center of mass displacement
+	volatile float displacement = (left_pos + right_pos)/(float)2;						//Center of mass displacement
 	volatile float sin, cos;
 
-	angle += (right_pos - left_pos) / ((float)WHEEL_DIST);	//angle between the x axis and the forward pointing vector of the puck
+	alpha += (left_pos - right_pos) / ((float)WHEEL_DIST);	//angle between the x axis and the forward pointing vector of the puck
 
 	/*
 	 * Due to floating point intrinsic arithmetics, it generally never gives a zero
 	 */
-	if(angle < 0.001){
-		angle = 0;
+
+	if((alpha >= 0 && alpha < 0.1) || (alpha <= 0 && alpha > -0.1)){
+		alpha = 0;
 	}
-	if(displacement < 0.001){
+	if((displacement < 1 && displacement >= 0) || (displacement > -1 && displacement <= 0)){
 		displacement = 0;
 	}
-	cos = arm_cos_f32(angle);
-	sin = arm_sin_f32(angle);
+	cos = arm_cos_f32(alpha);
+	sin = arm_sin_f32(alpha);
 
-	pos_x += displacement*cos;
-	pos_y += displacement*sin;
+	/*
+	 * if both pos are equal, the angle should not be updated
+	 */
+	if(left_pos != right_pos){
+		update_orientation(cos, sin);
+	}
 
-	chprintf((BaseSequentialStream *)&SD3, " %f \r \n", angle);
+	position[X_AXIS] += displacement*sin;
+	position[Y_AXIS] += displacement*cos;
+
+	//chprintf((BaseSequentialStream *)&SD3, " %f \r \n", alpha);
+}
+
+/*
+ * Basically a polar to cartesian transform, we consider it to be unitary
+ * as we don't care for the magnitude (for now). The orientation is of course
+ * a 2D vector centered in puck and pointing towards the camera/TOF direction
+ */
+void update_orientation(float cos, float sin){
+	orientation[X_AXIS] = sin;
+	orientation[Y_AXIS] = cos;
+	return;
+}
+
+
+/*
+ * move_to_target. we don't calculate the angle, as it implies calculating
+ * acos and asin, which are not given by the dsp or the fpu. Therefore, we
+ * try to close the angle by turning little by little until cos alpha is
+ * more or less equal to one
+ */
+
+void move_to_target(uint8_t *arrived){
+//	chprintf((BaseSequentialStream *)&SD3, "%f %f \r \n", position[0], position[1]);
+	volatile float move_l = 0, move_r = 0;
+	volatile float cos_alpha = 0;			//Cos of the angle between the orientation and the dist vector
+	volatile float dist[2] = {0,0};					//Vector between puck and target point
+	volatile float mag = 0;
+
+	arm_sub_f32(target, position, dist, 2);
+	arm_dot_prod_f32(orientation, dist, 2, &cos_alpha);
+	/*
+	 * Orientation is unitary, so we divide by dist magnitude only
+	 */
+	arm_cmplx_mag_f32(dist, &mag,1);
+
+	if(mag < MIN_DISTANCE){
+		*arrived = 1;
+		chprintf((BaseSequentialStream *)&SD3, " Arrived \r \n");
+		return;
+	}else{
+		cos_alpha = cos_alpha/mag;
+		chprintf((BaseSequentialStream *)&SD3, "%f \r \n", mag);
+		if( (cos_alpha < (1-ANGLE_EPSILON) && (cos_alpha > 0)) ||
+				(cos_alpha > (ANGLE_EPSILON - 1) && (cos_alpha < 0)) ){
+			if(position[X_AXIS] >= 0){
+				move_l = 5;
+				move_r = -5;
+			}else{
+				move_l = -5;
+				move_r = 5;
+			}
+		}else if ((cos_alpha >= (1 - ANGLE_EPSILON))){
+			move_l = 5;
+			move_r = 5;
+		}
+		/*
+		 * move() is called in the end because the move positions for each
+		 * wheel are calculated before
+		 */
+		move(move_l, move_r);
+		return;
+	}
 }
 
 
@@ -218,9 +315,9 @@ void init_pathing(){
 	proximity_start();
 	calibrate_ir();
 
-	pid.Kd = 0.5;
+	pid.Kd = 0.0001;
 	pid.Ki = 0;
-	pid.Kp = 2;
+	pid.Kp = 1;
 
 	arm_pid_init_f32(&pid, 0);
 
