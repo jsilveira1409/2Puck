f1709dba42d5b349bca9c01e823733cdac1069ca
jsilveira <joaquim.silveira@epfl.ch>
2022-04-27 17:01:27 +0200

    Not working, trying to reorganize code to make it more modular
    The distance between the puck and the target is only calculated when ir_index=none, which is not great. trying to put it before the switch case.

--- 2Puck/pathing.h
+++ 2Puck/pathing.h
@@ -4,5 +4,5 @@
 void move( float right_pos, float left_pos);
 uint8_t radar(int* ir_max);
 void register_path(volatile float left_pos,volatile  float right_pos);
-
+float distance_to_target(float* cos_alpha);
 #endif
