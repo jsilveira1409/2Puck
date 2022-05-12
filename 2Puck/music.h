79df189881a7c8a307c6a028ab4150898e3898b9
Karl Khalil <52137069+kkhalil7@users.noreply.github.com>
2022-05-12 09:08:43 +0200

    Refactor + implement msg_t between music and game

--- 2Puck/music.h
+++ 2Puck/music.h
@@ -16,9 +16,10 @@ song_selection_t music_init(void);
 void music_stop(void);
 void play_song(uint8_t index);
 void stop_song(void);
+void music_listen(uint8_t recording_size);
 void set_recording(uint8_t *data);
 void wait_finish_music(void);
-int16_t get_score(void);
+//int16_t get_score(void);
 song_selection_t choose_random_song(void);
 msg_t music_send_freq(float freq);
 
