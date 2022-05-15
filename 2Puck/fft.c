9300a0fef1b19030519da3c6d62b3b0a92f130be
jsilveira <joaquim.silveira@epfl.ch>
2022-05-15 15:23:20 +0200

    fft documented

--- 2Puck/fft.c
+++ 2Puck/fft.c
@@ -1,17 +1,27 @@
+/*
+ * fft.h
+ *
+ * 	Created on:
+ * 	Authors: Francesco Mondada
+ * 		     Daniel Burnier
+ */
+
 #include "ch.h"
 #include "hal.h"
 #include <main.h>
 #include <math.h>
 #include <fft.h>
-
 #include <arm_math.h>
 #include <arm_const_structs.h>
 
-static arm_rfft_fast_instance_f32 fft_handler;
-
 /*
-*	Wrapper to call a very optimized fft function provided by ARM
-*	which uses a lot of tricks to optimize the computations
+ * @brief 		FFT function calls
+ * @details		Wrapper to call a very optimized fft function provided by ARM
+ * 				which uses a lot of tricks to optimize the computations
+ *
+ * @param[in] size 					size of the complex buffer
+ * @param[in,out] complex_buffer	complex buffer containing the time-domain data
+ * 									on which the FFT is applied.
 */
 void doCmplxFFT_optimized(uint16_t size, float* complex_buffer){
 	/*
@@ -24,12 +34,4 @@ void doCmplxFFT_optimized(uint16_t size, float* complex_buffer){
 	  else if (size == 4096)
 		arm_cfft_f32(&arm_cfft_sR_f32_len4096, complex_buffer, 0, 1);
 
-}
-
-void doRealFFT_optimized(float* in_buffer, float* out_buffer){
-	arm_rfft_fast_f32(&fft_handler, in_buffer, out_buffer, 0);
-}
-
-void init_rfft_handler(uint16_t fft_size){
-	arm_rfft_fast_init_f32(&fft_handler, fft_size);
 }
--- 2Puck/fft.h
+++ 2Puck/fft.h
@@ -1,3 +1,11 @@
+/*
+ * fft.h
+ *
+ * 	Created on:
+ * 	Authors: Francesco Mondada
+ * 		     Daniel Burnier
+ */
+
 #ifndef FFT_H
 #define FFT_H
 #include "arm_math.h"
@@ -6,5 +14,5 @@
 void init_rfft_handler(uint16_t fft_size);
 void doCmplxFFT_optimized(uint16_t size, float* complex_buffer);
 
-void doRealFFT_optimized(float* in_buffer, float* out_buffer);
+
 #endif /* FFT_H */
